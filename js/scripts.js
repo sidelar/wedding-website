15f99b03b331efebe4947611242e042d129d69e5
sweinzimer <weinzims@oregonstate.edu>
2020-08-08 08:56:07 -0400

    update page

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 37.599865, lng: -77.817753};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
